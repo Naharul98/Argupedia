c04499f880253dd601d7e9d401357d68eec0a659
Naharul Hayat <37085164+Naharul98@users.noreply.github.com>
2019-11-22 20:50:34 +0000

    add post detail page

--- argupedia/urls.py
+++ argupedia/urls.py
@@ -3,10 +3,13 @@ from django.urls import path
 from django.views.generic.base import TemplateView
 
 from .views import HomeView
+from .views import PostsDetailView
+
 
 logged_users_redirect = user_passes_test(lambda u: u.is_anonymous, "/")
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path("posts/<int:pk_post>/", PostsDetailView.as_view(), name="posts-detail-view"),
 
 ]
--- argupedia/views.py
+++ argupedia/views.py
@@ -9,6 +9,7 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 class HomeView(View):
+
     def rebuild_tree(self, request, root_nodes):
         paginator = Paginator(root_nodes, settings.PAGINATE_ENTRIES_BY)
         page = request.GET.get("page")
@@ -36,3 +37,11 @@ class HomeView(View):
         root_nodes = root_nodes.annotate(overall_votes=(Count("upvotes") - Count("downvotes"))).order_by("-overall_votes")
         queryset = self.rebuild_tree(request, root_nodes)
         return render(request, "home.html", {"entries": queryset})
+
+class PostsDetailView(View):
+    def get(self, request, pk_post):
+        subtree = Entry.objects.get(pk=pk_post)
+        title = subtree.title
+        root_nodes = subtree.get_descendants(include_self=True)
+        root_nodes = root_nodes.annotate(overall_votes=(Count("upvotes") - Count("downvotes"))).order_by("-overall_votes")
+        return render(request, "post_detail.html", {"entries": root_nodes, "title" : title})
