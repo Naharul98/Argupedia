d5038a86a4393c9aefae4c1cb57886ff06f73bb4
Naharul Hayat <37085164+Naharul98@users.noreply.github.com>
2020-03-09 23:49:41 +0000

    dope shit

--- argupedia/views.py
+++ argupedia/views.py
@@ -16,33 +16,19 @@ import json
 
 # Create your views here.
 class HomeView(View):
+    def get(self, request):
+        root_nodes = Entry.objects.root_nodes()
+        root_nodes = root_nodes.annotate(overall_votes=(Count("upvotes") - Count("downvotes"))).order_by("-overall_votes")
 
-    def rebuild_tree(self, request, root_nodes):
-        paginator = Paginator(root_nodes, settings.PAGINATE_ENTRIES_BY)
-        page = request.GET.get("page")
-        queryset = []
+        page = request.GET.get('page', 1)
+        paginator = Paginator(root_nodes, 5)
         try:
             queryset = paginator.page(page)
         except PageNotAnInteger:
             queryset = paginator.page(1)
         except EmptyPage:
             queryset = paginator.page(paginator.num_pages)
-        new_queryset = []
-        for node in queryset.object_list:
-            new_queryset.append(node)
-            for descendant in node.get_descendants():
-                if descendant.level >= 9:
-                    if descendant.level == 9:
-                        new_queryset[-1].has_hidden_children = True
-                    continue
-                new_queryset.append(descendant)
-        queryset.object_list = new_queryset
-        return queryset
 
-    def get(self, request):
-        root_nodes = Entry.objects.root_nodes()
-        root_nodes = root_nodes.annotate(overall_votes=(Count("upvotes") - Count("downvotes"))).order_by("-overall_votes")
-        queryset = self.rebuild_tree(request, root_nodes)
         return render(request, "home.html", {"entries": queryset})
 
 class PostsDetailView(View):
@@ -50,7 +36,7 @@ class PostsDetailView(View):
         subtree = Entry.objects.get(pk=pk_post)
         title = subtree.title
         root_nodes = subtree.get_descendants(include_self=True)
-        root_nodes = root_nodes.annotate(overall_votes=(Count("upvotes") - Count("downvotes"))).order_by("-overall_votes")
+        root_nodes = root_nodes.annotate(overall_votes=(Count("upvotes") - Count("downvotes")))
         return render(request, "post_detail.html", {"entries": root_nodes, "title" : title, "root_post_id" : pk_post, "is_parent": (subtree.parent == None)})
 
 class MyDiscussionsView(DetailView):
